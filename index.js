3b9708c21d3e475506646bf4ec65dcf2f403ede4
archanabr05 <61489377+archanabr05@users.noreply.github.com>
2023-03-26 18:09:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,44 +2,57 @@
  * Required External Modules
  */
 
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-const CryptoJS = require("crypto-js");
+const CryptoJS = require('crypto-js');
 
 /**
  * App Variables
  */
 
 const app = express();
-const port = process.env.PORT || "8000";
-app.use(bodyParser.urlencoded({
+const port = process.env.PORT || '8000';
+app.use(
+  bodyParser.urlencoded({
     extended: true,
-    limit: '25mb'
-}));
-app.use(bodyParser.json({
-    limit: '25mb'
-}));
+    limit: '25mb',
+  })
+);
+app.use(
+  bodyParser.json({
+    limit: '25mb',
+  })
+);
 
 /**
  *  App Configuration
  */
 
-app.set("views", path.join(__dirname, "views"));
-app.set('view engine','ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-})
+  res.sendFile(__dirname + '/views/index.html');
+});
 
-app.post("/myServer", (req, res) => {
-  const { data, key } = req.body;
-  // console.log(req, req.body)
-  // const encrypted = CryptoJS.AES.encrypt(data, key);
-  // save encrypted file to a folder;
-  res.end('File Upload Successfull');
+app.get('/encryptionKey', (req, res) => {
+  res.json({ data: '12345' });
+});
+
+app.post('/myServer', (req, res) => {
+  const requestData = JSON.stringify(req.body);
+  fs.writeFile('./uploads/save.txt', requestData, err => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else {
+      console.log(`Request data saved!`);
+      res.sendStatus(200);
+    }
+  });
 });
 
 /**
